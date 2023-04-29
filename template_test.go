34da24775ba46b8fdd967f1cf42bf9b696c4e001
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-04-29 11:56:16 +0700

    add parse file template

--- template_test.go
+++ template_test.go
@@ -1,6 +1,7 @@
 package goweb
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"io"
@@ -23,6 +24,73 @@ func TestTemplateHtml(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	SimpleHTML(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+func TemplateParseFile(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./views/about.html"))
+
+	if err := tmpl.ExecuteTemplate(w, "about.html", "Hello About"); err != nil {
+		panic(err)
+	}
+}
+
+func TestTemplateParseFile(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/about", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateParseFile(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func TemplateGlobalParseFile(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseGlob("./views/*.html")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tmp.ExecuteTemplate(w, "about.html", "Hello About"); err != nil {
+		panic(err)
+	}
+}
+
+func TestTemplateGlobalParseFile(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/about", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateGlobalParseFile(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+//go:embed views/*.html
+
+var views embed.FS
+
+func TemplateEmbed(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFS(views, "views/*.html")
+	if err != nil {
+		panic(err)
+	}
+	if err := tmpl.ExecuteTemplate(w, "dashboard.html", "Welcome to Dashboard"); err != nil {
+		panic(err)
+	}
+}
+
+func TestTempateEmbed(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/about", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateEmbed(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
